cc8fc558030605c203b92948e3e0724e7341696e
dannynoordamdev <dannynoordam.dev@gmail.com>
2025-04-13 21:06:11 +0200

    init client with correct proj port

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const fetchApi = () =>{
+    // Write this function to fetch our test API of our asp.net core server.
+    alert("API!");
+  }
 
   return (
     <>
       <h1>RPi5 Dashboard</h1>
+      <p>Work in progress..</p>
+      <p>In the meantime, test this button:</p>
+      <button onClick={fetchApi}>Trigger API</button>
     </>
   )
 }
