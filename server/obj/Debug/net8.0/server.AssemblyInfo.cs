cfb5b16f1981e3e5e71dfc4d4999ba717ba6e7b3
dannynoordamdev <dannynoordam.dev@gmail.com>
2025-04-21 13:09:59 +0200

    dashboard effects

--- server/obj/Debug/net8.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net8.0/server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55fe04709515f1eb6569a648cabb7335c9e93e94")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91c982be550f1aa5403cebe3673991037c2e80aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
