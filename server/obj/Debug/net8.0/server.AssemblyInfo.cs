e369dff14ab24741de0e533c386d5520322b9ab9
dannynoordamdev <dannynoordam.dev@gmail.com>
2025-04-21 14:25:17 +0200

    fix styling

--- server/obj/Debug/net8.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net8.0/server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec3bd2327708bf743c36f5449c8bbb5ee4c74339")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bd5a34906cc825648c109061c4b285ad2d17c71")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
